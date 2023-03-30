e07c9778fe3329b5cec707757dbb5606f90502db
Karen Grigoryan <kgrigoryan@dsnci.com>
2023-03-31 01:09:19 +0300

    reset

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,32 @@
 import { NestFactory } from "@nestjs/core";
-import * as express from 'express';
-import * as http from 'http';
-import * as https from 'https';
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "./pipes/validation.pipe";
 import * as fs from "fs";
-import { ExpressAdapter } from "@nestjs/platform-express";
-
-const cors = require('cors');
 
 async function start() {
   const PORT = process.env.PORT || 5000;
+  let app;
 
-  const httpsOptions = {
-    key: fs.readFileSync(
-      "./certs/priv.pem"
-    ),
-    cert: fs.readFileSync(
-      "./certs/cert.pem"
-    ),
-  };
-
-  const server = express();
-
-  server.use(cors());
-
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(server),
-  );
-  await app.init();
+  if (process.env.SSL === "true") {
+    app = await NestFactory.create(AppModule, {
+      httpsOptions: {
+        key: fs.readFileSync(
+          "../../../etc/letsencrypt/live/badteacher-tech.ru/privkey.pem"
+        ),
+        cert: fs.readFileSync(
+          "../../../etc/letsencrypt/live/badteacher-tech.ru/cert.pem"
+        ),
+      },
+    });
+  } else {
+    app = await NestFactory.create(AppModule);
+  }
 
   app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
 
-  http.createServer(server).listen(7071);
-  https.createServer(httpsOptions, server).listen(PORT);
+  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 
 start();
