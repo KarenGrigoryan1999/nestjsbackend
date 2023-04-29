e964a5b3677d338061c67eb2afbc319a92bc2b9e
Karen Grigoryan <kgrigoryan@dsnci.com>
2023-04-29 21:14:40 +0300

    init

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -4,8 +4,7 @@ export class UpdateUserDto {
   @Length(1, 4, { message: "Не передан id юзера" })
   readonly id: number;
 
-  @IsEmail({}, { message: "Введите корректный email" })
-  readonly email: string;
+  readonly email?: string;
 
   @Length(2, 20, { message: "Имя должно быть от 2 до 20 символов" })
   readonly name: string;
